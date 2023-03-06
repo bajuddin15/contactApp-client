35b5cd5c00f9ea22690465b917d3eaea29155fd9
Bajuddin Khan <84461218+bajuddin15@users.noreply.github.com>
2023-03-06 22:35:55 +0530

    Update userActions.js

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -23,7 +23,7 @@ export const login = (email, password) => async (dispatch) => {
     };
 
     const { data } = await axios.post(
-      "/api/users/login",
+      "https://contact-app-server-db3w.onrender.com/api/users/login",
       { email, password },
       config
     );
@@ -58,7 +58,7 @@ export const register = (name, email, password, pic) => async (dispatch) => {
     };
 
     const { data } = await axios.post(
-      "/api/users",
+      "https://contact-app-server-db3w.onrender.com/api/users",
       { name, pic, email, password },
       config
     );
@@ -94,7 +94,7 @@ export const updateProfile = (user) => async (dispatch, getState) => {
       },
     };
 
-    const { data } = await axios.post("/api/users/profile", user, config);
+    const { data } = await axios.post("https://contact-app-server-db3w.onrender.com/api/users/profile", user, config);
 
     dispatch({ type: USER_UPDATE_SUCCESS, payload: data });
 
